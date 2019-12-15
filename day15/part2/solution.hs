caa03f21092b07c7dde34ce79c53e5cbe6266466
Francis J Sirizzotti <francis@sirizzotti.info>
2019-12-15 14:26:35 -0500

    day 15

---
+++ day15/part2/solution.hs
@@ -0,0 +1,287 @@
+import System.Environment
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import Data.List
+
+class VirtualIO a where
+  readInput :: a -> (Int, a)
+  readInput _ = error "No input function supplied"
+  writeOutput :: Int -> a -> a
+  writeOutput _ _ = error "No output function supplied"
+
+type Memory = Map.Map Int Int
+data State = Running | Halted | Paused deriving (Enum, Show, Eq)
+
+data Computer a = Computer {
+  memory :: Memory
+  , insPointer :: Int
+  , state :: State
+  , relativeBase :: Int
+  , io :: a
+} deriving (Show)
+
+pauseOnOutput :: Bool
+pauseOnOutput = False
+
+boot :: Computer ListIO
+boot = Computer Map.empty 0 Running 0 (ListIO [] [])
+
+shouldBreak :: Computer a -> Bool
+shouldBreak (Computer _ _ Running _ _) = False
+shouldBreak x = True
+
+readMem :: Int -> Memory -> Int
+readMem = Map.findWithDefault 0
+
+write :: Int -> Int -> Computer a -> Computer a
+write address value computer
+  | address >= 0 = computer { memory = Map.insert address value mem }
+  where mem = memory computer
+
+rd :: VirtualIO a => Computer a -> (Int, Computer a)
+rd computer = 
+  let (value, remaining) = readInput $ io computer
+    in (value, computer { io = remaining, state = if value == -1 then Halted else Running})
+
+prnt :: VirtualIO a => Int -> Computer a -> Computer a
+prnt value computer = computer { 
+    io = writeOutput value $ io computer
+    , state = if pauseOnOutput then Paused else state computer
+  }
+
+advance :: Int -> Computer a -> Computer a
+advance instructions computer = 
+  let oldPointer = insPointer computer
+    in computer { insPointer = oldPointer + instructions }
+
+getInstruction :: Computer a -> Int
+getInstruction (Computer memory insPointer _ _ _) = readMem insPointer memory
+
+getParams :: Int -> Computer a -> [Int]
+getParams count (Computer memory insPointer _ _ _) = [ readMem (insPointer + i) memory | i <- [1..count]]
+
+data ParamMode = Position | Immediate | Relative | RelWrite deriving (Enum, Show, Eq)
+
+data Parameter = Parameter {
+  mode :: ParamMode
+  , param :: Int
+} deriving (Show)
+
+getValue :: Parameter -> Computer a -> Int
+getValue (Parameter Immediate val) _ = val
+getValue (Parameter Position address) computer = readMem address (memory computer)
+getValue (Parameter Relative offset) (Computer mem _ _ base _) = readMem (base + offset) mem
+getValue (Parameter RelWrite offset) (Computer mem _ _ base _) = base + offset
+
+type OpFunc a = [Int] -> Computer a -> Computer a
+
+mathOp :: (Int -> Int -> Int) -> [Int] -> Computer a -> Computer a
+mathOp f [lhs, rhs, address] = write address (lhs `f` rhs)
+
+add :: OpFunc a
+add = mathOp (+)
+
+mult :: OpFunc a
+mult = mathOp (*)
+
+inputOp :: VirtualIO a => OpFunc a
+inputOp [address] computer = 
+    let (value, postRead) = rd computer
+      in write address value postRead
+
+outputOp :: VirtualIO a => OpFunc a
+outputOp [value] = prnt value
+
+halt :: OpFunc a
+halt [] computer = computer { state = Halted }
+
+cndJmp :: Bool -> [Int] -> Computer a -> Computer a
+cndJmp goal [arg, target] computer
+  | (0 /= arg) == goal = computer { insPointer = target - 3 }
+  | otherwise = computer
+
+jmpT :: OpFunc a
+jmpT = cndJmp True
+
+jmpF :: OpFunc a
+jmpF = cndJmp False
+
+cmpOp :: (Int -> Int -> Bool) -> [Int] -> Computer a -> Computer a
+cmpOp cmp = mathOp (curry (fromEnum.(uncurry cmp))) 
+
+lt :: OpFunc a
+lt = cmpOp (<)
+
+eq :: OpFunc a
+eq = cmpOp (==)
+
+adjBase :: OpFunc a
+adjBase [offset] computer = computer { relativeBase = offset + (relativeBase computer) }
+
+data Operation a = Operation {
+  modalParams
+  , numParams :: Int
+  , f :: OpFunc a
+}
+
+writeMode :: ParamMode -> ParamMode
+writeMode Position = Immediate
+writeMode Relative = RelWrite
+
+getModes :: Int -> Operation a -> [ParamMode]
+getModes instruction (Operation modal total _) = 
+    let bits = reverse (show (instruction `quot` 100)) ++ repeat '0'
+        ints = [ read [i] :: Int | i <- bits ]
+        asEnum = [ toEnum i :: ParamMode | i <- ints ]
+        actual = (take modal asEnum) ++ (map writeMode (drop modal asEnum))
+      in take total actual
+
+getOp :: VirtualIO a => Int -> Operation a
+getOp  1 = Operation 2 3 add
+getOp  2 = Operation 2 3 mult
+getOp  3 = Operation 0 1 inputOp
+getOp  4 = Operation 1 1 outputOp
+getOp  5 = Operation 2 2 jmpT
+getOp  6 = Operation 2 2 jmpF
+getOp  7 = Operation 2 3 lt
+getOp  8 = Operation 2 3 eq
+getOp  9 = Operation 1 1 adjBase
+getOp 99 = Operation 0 0 halt
+getOp code
+  | code > 99 = getOp (code `mod` 100)
+
+parseProgram :: String -> Memory
+parseProgram str = 
+  let spaceSeparated = [if c == ',' then ' ' else c | c <- str]
+      instructions = [read i :: Int | i <- words spaceSeparated]
+    in Map.fromDistinctAscList $ zip [0..] instructions
+
+executeInstruction :: VirtualIO a => Computer a -> Computer a
+executeInstruction computer = 
+  let instruction = getInstruction computer
+      op = getOp instruction
+      paramCt = numParams op
+      paramValues = getParams paramCt computer
+      paramModes = getModes instruction op
+      parameters = map (((flip getValue) computer).(uncurry Parameter)) (zip paramModes paramValues)
+      afterOp = (f op) parameters computer
+    in advance (paramCt + 1) afterOp
+
+executeProgram :: VirtualIO a => Computer a -> Computer a
+executeProgram comp
+  | fin = comp
+  | otherwise = executeProgram (executeInstruction comp)
+  where fin = shouldBreak comp
+
+data ListIO = ListIO {
+    i :: [Int]
+    , o :: [Int]
+} deriving (Show)
+
+instance VirtualIO ListIO where
+   readInput (ListIO i o) = (head i, ListIO (tail i) o)
+   writeOutput v (ListIO i o) = ListIO i (v:o)
+
+data OxygenIO = OxygenIO {
+  position :: (Int, Int)
+  , known :: Map.Map (Int, Int) Int
+  , lastIns :: Int
+}
+initialize :: OxygenIO
+initialize = OxygenIO (0,0) (Map.singleton (0,0) 1) 1
+
+nextPosition :: (Int, Int) -> Int -> (Int, Int)
+nextPosition (x, y) d = case d of
+  1 -> (x, y + 1)
+  2 -> (x, y - 1)
+  3 -> (x - 1, y)
+  4 -> (x + 1, y)
+
+adjacent :: (Int, Int) -> [(Int, Int)]
+adjacent start = map (nextPosition start) [1..4]
+
+bfs :: [[(Int, Int)]] -> Set.Set (Int, Int) -> Map.Map (Int, Int) Int -> Maybe (Int, Int)
+bfs [] _ _ = Nothing
+bfs (c:rest) seen lookup = case Map.lookup (head c) lookup of
+  Nothing -> Just $ last c
+  Just 0 -> bfs rest seen lookup
+  _ -> let possible = adjacent $ head c
+           notSeen = filter ((flip Set.notMember) seen) possible
+           seen' = foldl (flip Set.insert) seen notSeen
+           candidates = rest ++ map (:c) notSeen
+         in bfs candidates seen' lookup
+
+findNext :: (Int, Int) -> Map.Map (Int, Int) Int -> Maybe (Int, Int)
+findNext start lookup =
+  let dirs = adjacent start
+      seen = Set.fromList dirs
+      candidates = map (:[]) dirs
+    in bfs candidates seen lookup
+
+calcDir :: (Int, Int) -> (Int, Int) -> Int
+calcDir (x1, y1) (x2, y2)
+  | x1 == x2 && y2 == y1 + 1 = 1
+  | x1 == x2 && y2 == y1 - 1 = 2
+  | x1 == x2 + 1 && y2 == y1 = 3
+  | x1 == x2 - 1 && y2 == y1 = 4
+
+foundToChar :: Int -> Char
+foundToChar (-1) = ' '
+foundToChar  0 = '#'
+foundToChar  1 = '.'
+foundToChar  2 = '@'
+
+toStr :: (Int, Int) -> Map.Map (Int, Int) Int -> [Char]
+toStr curr known
+  | Map.size known == 0 = "?"
+  | otherwise = 
+    let ps = Map.keys known
+        minX = minimum . map fst $ ps
+        maxX = maximum . map fst $ ps
+        minY = minimum . map snd $ ps
+        maxY = maximum . map snd $ ps
+        coords = [ [ (x, y) | x <- [minX..maxX] ] | y <- reverse [minY..maxY] ]
+        toChar p = if p == curr then 'D' else foundToChar (Map.findWithDefault (-1) p known)
+        txt = map (map toChar) coords
+      in unlines txt 
+
+instance VirtualIO OxygenIO where
+  readInput o@OxygenIO{position=position, known=known} = case findNext position known of
+    Nothing -> (-1, o { lastIns = -1 })
+    Just next -> let dir = calcDir position next in (dir, o { lastIns = dir})
+  writeOutput v (OxygenIO pos known lastIns) =
+    let next = nextPosition pos lastIns
+        known' = Map.insert next v known
+        pos' = if v == 0 then pos else next
+      in OxygenIO pos' known' lastIns
+
+fill :: Int -> [(Int, Int)] -> Set.Set (Int, Int) -> Map.Map (Int, Int) Int -> Int
+fill n [] _ _ = n - 1
+fill n curr seen lookup = 
+  let seen' = foldl (flip Set.insert) seen curr
+      spread = concat $ map adjacent curr
+      new = filter (flip Set.notMember seen') spread
+      checker k = Map.findWithDefault 0 k lookup
+      open = filter ((/=0).checker) new
+    in fill (n + 1) open seen' lookup
+--pathTo :: (Int, Int) -> [[(Int, Int)]] -> Set.Set (Int, Int) -> Map.Map (Int, Int) Int -> [(Int, Int)]
+--pathTo target (c:rest) seen lookup
+--  | target == head c = c
+--  | otherwise =
+--    let possible = adjacent $ head c
+--        checker k = Map.findWithDefault 0 k lookup
+--        open = filter ((/=0).checker) possible
+--        notSeen = filter ((flip Set.notMember) seen) open
+--        seen' = foldl (flip Set.insert) seen notSeen
+--        candidates = rest ++ map (:c) notSeen
+--      in pathTo target candidates seen' lookup
+
+main = do
+  args <- getArgs
+  let fileName = head args
+  contents <- readFile fileName
+  let program = parseProgram contents
+  let result = known $ io $ executeProgram boot { memory = program, io = initialize }
+  let oxySystem = head $ Map.keys $ Map.filter (==2) result
+  -- let path = pathTo oxySystem [[(0,0)]] (Set.singleton (0,0)) result
+  print $ fill 0 [oxySystem] Set.empty result
